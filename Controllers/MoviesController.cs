44d9adf58ea7f5636fa626c8db47c485ae4f3108
maschinedreams <gabrielle.clima23@gmail.com>
2019-09-27 18:16:12 -0300

    tudo ok

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+
+        {
+            var movie = new Movies() {Name = "Shrek"};
+            return View(movie);
+        }
+        public ActionResult Edit(int id) 
+        {
+            return Content("id=" + id);
+        }
+        public ActionResult Index(int? pageIndex, string SortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(SortBy));
+            SortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, SortBy));
+        }
+    }
+}
---
+++ RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using Glimpse.AspNet.Tab;
+using Microsoft.AspNetCore.Routing;
+using System.Web.Mvc;
+
+namespace WebApplication
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.Ignore("{resource}.axd/{*pathInfo}");
+
+            Routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+
+       
+    }
+}
